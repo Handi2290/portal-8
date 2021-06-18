f5e718f8dbcae4ce848fb96a4394c95861f5b5b9
Handi2290 <handikomara22@gmail.com>
2021-06-18 10:50:45 +0700

    Update Composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e222b66b01173593d328c8bf5222e021b5c727d6',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e222b66b01173593d328c8bf5222e021b5c727d6',
+    ),
+  ),
+);
